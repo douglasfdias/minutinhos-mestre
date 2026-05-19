fcd7c933083effef99e69e09274c5e51c3df4583
douglasfdias <douglasdias.dfd@gmail.com>
2026-05-18 23:11:55 -0300

    Update sw-mestre.js

--- sw-mestre.js
+++ sw-mestre.js
@@ -1,5 +1,5 @@
 // sw-mestre.js — Service Worker — Painel do Mestre
-const CACHE_NAME = 'minutinhos-mestre-v10';
+const CACHE_NAME = 'minutinhos-mestre-v11';
 
 const ASSETS = [
   '/minutinhos-mestre/',
